9f3777483472c562cb17b932a7dab33bd162b769
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-13 16:48:39 +0200

    exo3 fini

--- src/test/java/java8/ex03/Stream_03_Test.java
+++ src/test/java/java8/ex03/Stream_03_Test.java
@@ -54,7 +54,7 @@ public class Stream_03_Test {
         // TODO Séparer la liste des pizzas en 2 ensembles :
         // TODO true -> les pizzas dont le nom commence par "L"
         // TODO false -> les autres
-        Map<Boolean, List<Pizza>> result = null;
+        Map<Boolean, List<Pizza>> result = pizzas.stream().collect(Collectors.partitioningBy(p->p.getName().startsWith("L")));
 
         assertThat(result.get(true), hasSize(6));
         assertThat(result.get(false), hasSize(2));
